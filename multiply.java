5b94fd16086c23d82efb75618f46844cc3429821
Nandakumar <nandkumargokavi@gmail.com>
2025-08-10 15:29:45 +0530

    multiply.java addedd

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int product = a * b;
+        System.out.println("The product is: " + product);
+    }   
+    
+}
